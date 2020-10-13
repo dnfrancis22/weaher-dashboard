c017effbddaf0473769e23ee7fb71b582385e83d
Dennis Francis <dnfrancis22@gmail.com>
2020-10-13 15:52:24 -0400

    created function to render the button based on user input.

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 $(document).ready(function () {
-  // This is our API key. Add your own API key between the ""
-  var APIKey = "100ea10c1f1db96d42a65493df31b77f";
 
-  // Here we are building the URL we need to query the database
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" +
-    APIKey + "&units=imperial";
+    var cities = ["Atlanta"];
 
-  // We then created an AJAX call
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(queryURL);
-    console.log(response);
+    function renderButtons () {
 
-    $(".city").html("<h3>" + response.name);
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    $(".temp").text("Temp: " + response.main.temp);
-    $(".uvIndex").text("UV Index: " + response.main.temp);
-    
+        for (var i = 0; i < cities.length; i++){
+            var button = $("<button>");
+            button.text(cities[i]);
+            button.addClass("searchBtn")
+            $("#buttonArea").prepend(button);
+        }
+
+    }
+
+  $("#button-addon2").on("click", function (event) {
+
+
+
+    var APIKey = "100ea10c1f1db96d42a65493df31b77f";
+
+    var city = $("#searchField").val();
+
+    // Here we are building the URL we need to query the database
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      APIKey +
+      "&units=imperial";
+
+    // We then created an AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(queryURL);
+      console.log(response);
+
+      $(".city").html("<h3>" + response.name);
+      $(".wind").text("Wind Speed: " + response.wind.speed);
+      $(".humidity").text("Humidity: " + response.main.humidity);
+      $(".temp").text("Temp: " + response.main.temp);
+      $(".uvIndex").text("UV Index: " + response.main.temp);
+    });
+
+    renderButtons();
   });
+  // This is our API key. Add your own API key between the ""
 });
