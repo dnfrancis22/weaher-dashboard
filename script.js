f15c52861db36a212cd8a063ff08b0db9bac36f4
Dennis Francis <dnfrancis22@gmail.com>
2020-10-13 12:53:33 -0400

    created ajax call for current weather and appended to the html elements.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // This is our API key. Add your own API key between the ""
+  var APIKey = "100ea10c1f1db96d42a65493df31b77f";
 
+  // Here we are building the URL we need to query the database
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" +
+    APIKey + "&units=imperial";
 
+  // We then created an AJAX call
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(queryURL);
+    console.log(response);
 
+    $(".city").html("<h3>" + response.name);
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".temp").text("Temp: " + response.main.temp);
+    $(".uvIndex").text("UV Index: " + response.main.temp);
+    
+  });
 });
